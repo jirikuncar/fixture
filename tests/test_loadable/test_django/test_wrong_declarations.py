656f4cd8db5a303202753a9b7debc2b17bd09631
Jiri Kuncar <jiri.kuncar@cern.ch>
2014-03-25 11:17:34 +0100

    Import and whitespace fixes

--- tests/test_loadable/test_django/test_wrong_declarations.py
+++ tests/test_loadable/test_django/test_wrong_declarations.py
@@ -1,8 +1,10 @@
+from __future__ import absolute_import
+
 from fixture import DjangoFixture, DataSet, style
 from fixture.exc import LoadError
 from nose.tools import raises, assert_raises
 
-from util import *
+from .util import *
 from fixture.examples.django_example.app import models
 
 class ReviewerData(DataSet):
@@ -22,7 +24,7 @@ class AuthorData(DataSet):
 
 dj_fixture = DjangoFixture(env=models, style=style.NamedDataStyle())
 
-  
+
 def test_wrong_relation_declaration():
     assert 'reviewers' in models.Book._meta.get_all_field_names()
     data = dj_fixture.data(BookData)
@@ -37,5 +39,5 @@ def test_invalid_m2m():
     assert_empty(models)
     data = dj_fixture.data(ReviewerData)
     assert_raises(LoadError, data.setup)
-    data.teardown()   
+    data.teardown()
 
